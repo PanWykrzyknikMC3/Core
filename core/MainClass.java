f4b6c09d9372eba5784f8d74d77f0e35215e361f
PanWykrzyknikMC3 <38790607+PanWykrzyknikMC3@users.noreply.github.com>
2018-05-30 08:43:30 +0200

    Create MainClass.java

---
+++ core/MainClass.java
@@ -0,0 +1,33 @@
+package core;
+
+public class MainClass extends JavaPlugin{
+	public void onEnable()
+	{
+		System.out.println("Ladowanie core...");
+	}
+	public void onDisable()
+	{
+		System.out.println("Wylaczanie core...");
+	}
+	List<String> help = this.getConfig().getStringList("help");
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		Player p = (Player) sender;
+    if(cmd.getName().equalsIgnoreCase("help")){
+			sender.sendMessage(help);
+		}
+    if(cmd.getName().equalsIgnoreCase("gamemode")){
+      if(args.length==0){
+      p.sendMessage(this.getConfig().getString("usage").replace("{USAGE}", "/gamemode <tryb> [gracz]").replace("&", "§"));
+      
+      }
+      if(args.length==1){
+        if(args[0].equals(0) | args[0].equalsIgnoreCase("s")){
+          p.setGameMode(GameMode.SURVIVAL);
+          p.sendMessage(this.getConfig().getString("gamemodeMsg").replace("{GAMEMODE}", "SURVIVAL").replace("&", "§"));
+        }
+      }
+    }
+	}
+	
+	}
